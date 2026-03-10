d7c877dbeb12961255753989c3c03c42898d551f
CharlotteBeecher <beechercharlotte@gmail.com>
2026-03-10 14:56:20 +0000

    changed to fruitsAPP

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const fruitRoutes = require('./routes/fruitRoutes')
 fruitsApp.use(express.json()) //use = do this for any http method
 fruitsApp.use('/fruits', fruitRoutes)
 
-app.use(cors())
-app.use(express.json())
-app.use('/fruits',fruits)
+fruitsApp.use(cors())
+fruitsApp.use(express.json())
 
 module.exports = fruitsApp

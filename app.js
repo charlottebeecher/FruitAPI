e720f6530a87fcc572a1e6400a85ee9c9fa71550
CharlotteBeecher <beechercharlotte@gmail.com>
2026-03-10 14:52:29 +0000

    added cors

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const fruitsApp = express()
 const fruitRoutes = require('./routes/fruitRoutes')
 
 fruitsApp.use(express.json()) //use = do this for any http method
 fruitsApp.use('/fruits', fruitRoutes)
+
+app.use(cors())
+app.use(express.json())
+app.use('/fruits',fruits)
 
 module.exports = fruitsApp
